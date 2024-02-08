0bcfa924e19be1abff759f7bba00f0f2dd8ac86e
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2024-02-08 21:54:33 +0100

    + fix form AddOffersForm

--- src/app/components/AddOffersForm/AddOffersForm.jsx
+++ src/app/components/AddOffersForm/AddOffersForm.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import { TextField, Select, MenuItem, Checkbox, FormControlLabel, Button, Grid, Typography, InputLabel, OutlinedInput, Chip, FormControl } from '@mui/material';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Container from '../Container/Container';
-import { jobType, proffesionTypesArray } from '@/utils/filterElements';
+import { courtTypes, jobType, proffesionTypesArray } from '@/utils/filterElements';
 import { Box } from '@mui/system';
 import { useTheme } from '@emotion/react';
+import { addOffers } from '@/redux/offers/operations';
+import { fetchProvinces } from '@/redux/terc/operations';
+import { fetchCities } from '@/redux/simc/operations';
+import { selectCities } from '@/redux/simc/selectors';
+import { selectProvinces } from '@/redux/terc/selectors';
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -30,45 +35,60 @@ function getStyles(name, proffesionType, theme) {
 const AddOffersForm = () => {
     const theme = useTheme();
     const dispatch = useDispatch();
-    const [title, setTitle] = useState('');
-    console.log(title) //.trim() .toLowerCase
-    const [description, setDescription] = useState('');
-    const [gotJobType, setGotJobType] = useState('');
-    const [proffesionTypes, setProffesionTypes] = useState([]);
-    const [court, setCourt] = useState('');
-    const [region, setRegion] = useState('');
-    const [city, setCity] = useState('');
-    const [postalCode, setPostalCode] = useState('');
-    const [streetAddress, setStreetAddress] = useState('');
-    const [date, setDate] = useState('');
-    const [time, setTime] = useState('');
-    const [email, setEmail] = useState('');
-    const [phone, setPhone] = useState('');
-    const [compensation, setCompensation] = useState('');
-    const [compensationAgreement, setCompensationAgreement] = useState(false);
-    const [vat, setVat] = useState(false);
+    const cities = useSelector(selectCities);
+    const provinces = useSelector(selectProvinces);
+    // for create object
+    const [title, setTitle] = useState(''); console.log('title :', title) //.trim() .toLowerCase
+    const [description, setDescription] = useState(''); console.log('description :', description)
+    const [gotJobType, setGotJobType] = useState(''); console.log('gotJobType :', gotJobType)
+    const [proffesionTypes, setProffesionTypes] = useState([]); console.log('proffesionTypes :', proffesionTypes)
+    const [court, setCourt] = useState(''); console.log('court :', court)
+    const [region, setRegion] = useState(''); console.log('region :', region)
+    const [city, setCity] = useState(''); console.log('city :', city)
+    const [postalCode, setPostalCode] = useState(''); console.log('postalCode :', postalCode)
+    const [streetAddress, setStreetAddress] = useState(''); console.log('streetAddress :', streetAddress)
+    const [date, setDate] = useState(''); console.log('date :', date)
+    const [time, setTime] = useState(''); console.log('time :', time)
+    const [email, setEmail] = useState(''); console.log('email :', email)
+    const [phone, setPhone] = useState(''); console.log('phone :', phone)
+    const [compensation, setCompensation] = useState(''); console.log('compensation :', compensation)
+    const [compensationAgreement, setCompensationAgreement] = useState(false); console.log('compensationAgreement :', compensationAgreement)
+    const [vat, setVat] = useState(false); console.log('vat :', vat)
+    const [filteredCities, setFilteredCities] = useState(null);
 
     // const capitalizeFirstLetter = (text) => {
     //     return text.charAt(0).toUpperCase() + text.slice(1);
     // };
+
+    useEffect(() => {
+        dispatch(fetchProvinces());
+        dispatch(fetchCities());
+    }, [dispatch]);
+    useEffect(() => {
+        const filteredCities =
+            cities.filter(city => city.woj === region.woj);
+        // console.log(filteredCities)
+        setFilteredCities(filteredCities)
+    }, [region, cities]);
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
         const offerData = {
             address: {
-                city: city,
+                city: city.name,
                 postalCod: postalCode,
-                province: region,
+                province: region.name,
                 // id
                 // simcId
                 street: streetAddress,
                 phone: phone,
             },
             // addressId: nanoid(),
-            // court: {
-            //     phone: phone,
-            // },
+            court: {
+                name: court.label,
+                phone: phone,
+            },
             // courtId
             createdDate: date,
             dateToDetermined: date === '' && true,
@@ -79,13 +99,13 @@ const AddOffersForm = () => {
             // nameOrCompany
             // offers:[],
             priceToDetermined: compensationAgreement,
-            proffesionTypes: [...proffesionTypes],
+            proffesionTypes: proffesionTypes.map(item => item.value),
             // status:
             time: time,
             title: title.trim(),
             vatInvoice: vat,
         };
-        console.log(offerData)
+        console.log('CREATED OBJECK :', offerData)
         dispatch(addOffers(offerData))
     };
 
@@ -154,12 +174,16 @@ const AddOffersForm = () => {
                                 fullWidth
                                 required
                                 value={proffesionTypes}
-                                onChange={(e) => setProffesionTypes(e.target.value)}
+                                onChange={(e) => {
+                                    setProffesionTypes(e.target.value);
+                                    // const selectedValues = e.target.value.map(item => item.value);
+                                    // setProffesionTypes(selectedValues);
+                                }}
                                 input={<OutlinedInput id="select-multiple-chip" label="Wymagane kwalifikacje" />}
                                 renderValue={(selected) => (
                                     <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                                        {selected.map((value) => (
-                                            <Chip key={value.name} label={value.name} />
+                                        {selected.map((value, index) => (
+                                            <Chip key={index} label={value.name} />
                                         ))}
                                     </Box>
                                 )}
@@ -183,35 +207,71 @@ const AddOffersForm = () => {
                 </Typography>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
-                        <TextField
-                            label="Court"
-                            variant="outlined"
-                            fullWidth
-                            required
-                            value={court}
-                            onChange={(e) => setCourt(e.target.value)}
-                        />
+                        <FormControl sx={{ minWidth: 200 }}>
+                            <InputLabel
+                                id="demo-court-select-label"
+                            >
+                                Sąd
+                            </InputLabel>
+                            <Select
+                                labelId="demo-court-select-label"
+                                id="demo-court-select"
+                                required
+                                fullWidth
+                                value={court}
+                                label="Sąd"
+                                onChange={(e) => setCourt(e.target.value)}
+                            >
+                                {courtTypes.map(type => {
+                                    return (
+                                        <MenuItem key={type.value} value={type}>{type.label}</MenuItem>
+                                    )
+                                })}
+                            </Select>
+                        </FormControl>
                     </Grid>
                     <Grid item xs={6}>
-                        <TextField
-                            label="Region"
-                            variant="outlined"
-                            fullWidth
-                            required
-                            value={region}
-                            onChange={(e) => setRegion(e.target.value)}
-                        />
+                        <FormControl sx={{ minWidth: 200 }}>
+                            <InputLabel id="province-name">Województwo</InputLabel>
+                            <Select
+                                labelId="province-name"
+                                id="province-dropdown-name"
+                                value={region}
+                                label="Województwo"
+                                fullWidth
+                                required
+                                onChange={(e) => setRegion(e.target.value)}
+                            >
+                                {provinces.map((province) => (
+                                    <MenuItem key={province.id} value={province}>
+                                        {province.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
                     </Grid>
-                    <Grid item xs={6}>
-                        <TextField
-                            label="City"
-                            variant="outlined"
-                            fullWidth
-                            required
-                            value={city}
-                            onChange={(e) => setCity(e.target.value)}
-                        />
-                    </Grid>
+
+                    {region && (
+                        <Grid item xs={6}>
+                            <FormControl sx={{ minWidth: 200 }}>
+                                <InputLabel id="city-name">Miejscowość</InputLabel>
+                                <Select
+                                    labelId="city-name"
+                                    id="city-dropdown-name"
+                                    value={city}
+                                    label="Miejscowość"
+                                    onChange={(e) => setCity(e.target.value)}
+                                >
+                                    <MenuItem value="">Wszystko</MenuItem>
+                                    {filteredCities?.map((city) => (
+                                        <MenuItem key={city.id} value={city}>
+                                            {city.name}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </Grid>
+                    )}
                     <Grid item xs={6}>
                         <TextField
                             label="Postal Code"
