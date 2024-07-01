d95706aaab7e6860441a6973ef1251752d393b5f
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2024-07-01 19:43:45 +0200

    Update [id].jsx

--- pages/job/[id].jsx
+++ pages/job/[id].jsx
@@ -1,14 +1,71 @@
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import { NextPage } from 'next';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectJobs } from '@/redux/jobs/selectors';
+import SharedLayout from '@/app/components/SharedLayout';
+import Container from '@/app/components/Container/Container';
+import { fetchAllJobs } from '@/redux/jobs/operations';
 
-const JobPage = ({ NextPage }) => {
-    const { asPath, pathname, query } = useRouter();
+const JobPage = () => {
+    const dispatch = useDispatch();
+    const router = useRouter();
+    const { id } = router.query;
+    const currentID = Number(id);
+    const allJobs = useSelector(selectJobs);
+    console.log(allJobs)
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (!id) return;
+
+        const getData = async () => {
+            try {
+                await dispatch(fetchAllJobs());
+            } catch (error) {
+                console.error('Error fetching jobs:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        getData();
+    }, [id, dispatch]);
+
+    const currentJob = allJobs ? allJobs.find((job) => job.id === currentID) : null;
+    console.log('current job :', currentJob)
+
+    //loader
+    if (loading) {
+        return (
+            <SharedLayout>
+                <Container>
+                    <div>Loading...</div>
+                </Container>
+            </SharedLayout>
+        );
+    }
+
     // console.log(asPath)
     // console.log(pathname)
     // console.log(query)
 
     return (
-        <div>JobPage</div>
+        <SharedLayout>
+            <Container>
+                {currentJob ? (
+                    <>
+                        <h1>{currentJob.title}</h1>
+                        <p>{currentJob.description}</p>
+                        <p>Location: {currentJob.address?.city}</p>
+                    </>
+                ) : (
+                    <div>Job not found</div>
+                )}
+
+            </Container>
+
+        </SharedLayout>
+
     )
 }
 
